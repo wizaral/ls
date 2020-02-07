c0351b40f2353c3df2f8e79f04977b8fc8a6d97b
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-02-07 23:13:42 +0200

    structs

--- LS/inc/structs.h
+++ LS/inc/structs.h
@@ -6,8 +6,57 @@
 
 // структура файла содержит все-все поля
 
-// структура с функциями содержит указатели на необходимые параметры вывода
+typedef struct s_file {
+    uint64_t inode;
+    uint64_t blocksize;
+    char *access;
+    uint64_t links;
+    char *user;
+    char *grp;
+    char *flags;
+    char *size;
+    char *time;
+    char *name;
+} t_file;
 
-// вспомогательная структура которая содержит необходимую инфу для вывода
+typedef enum e_time_type {
+    access = 1 << 0,
+    modification = 1 << 1,
+    change = 1 << 2,
+    creation = 1 << 3,
+} t_time_type;
+
+	// struct timespec st_atimespec;           /* time of last access */ \
+	// struct timespec st_mtimespec;           /* time of last data modification */ \
+	// struct timespec st_ctimespec;           /* time of last status change */ \
+	// struct timespec st_birthtimespec;       /* time of file creation(birth) */
+
+typedef struct s_print t_print;
 
 // структура с файлами и директориями
+
+typedef struct s_info {
+    bool dst;
+    t_print print;
+    t_vector files;
+    t_time_type time_type;
+
+    void (*out)(t_info *info);
+    void (*sort)(t_info *info);
+
+} t_info;
+
+// структура с функциями содержит указатели на необходимые параметры вывода
+
+struct s_print {
+    void (*print_inode)(t_info *info);
+    void (*print_bsize)(t_info *info);
+    void (*print_access)(t_info *info);
+    void (*print_user)(t_info *info);
+    void (*print_grp)(t_info *info);
+    void (*print_fflags)(t_info *info);
+    void (*print_size)(t_info *info);
+    void (*print_time)(t_info *info);
+    void (*print_name)(t_info *info);
+    void (*print_name_add)(t_info *info);
+};
---
+++ test.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <string.h>
+
+char *strnew(const int size) {
+    char *ptr = NULL;
+
+    if (size >= 0 && (ptr = (char *)malloc(size + 1)))
+        for (int i = 0; i <= size; ++i)
+            ptr[i] = '\0';
+    return ptr;
+}
+
+char *strjoin(char const *s1, char const *s2) {
+    if (s1 && s2) {
+        char *str = strnew(strlen(s1) + strlen(s2));
+        return str ? strcat(strcat(str, s1), s2) : NULL;
+    }
+    return (s1 || s2) ? (s1 ? strdup(s1) : strdup(s2)) : NULL;
+}
+
+int main() {
+    DIR *dir = opendir("/");
+    char *name;
+    struct stat _stat;
+    struct dirent *file = readdir(dir);
+
+    while (file) {
+        name = strjoin("/", file->d_name);
+        printf(":%s:", name);
+        stat(name, &_stat);
+        // printf("%s:%u:\n", fflagstostr(_stat.st_flags), _stat.st_flags);
+        printf("%llu:\n", _stat.st_ino);
+        file = readdir(dir);
+        free(name);
+        name = NULL;
+    }
+    closedir(dir);
+}
