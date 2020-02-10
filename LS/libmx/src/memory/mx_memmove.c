72c6540c2ce1b6fc37742e2daedda9b2f35f0951
Alex Master <alex.macromaster@outlook.com>
2020-02-10 03:21:12 +0200

    memory fixes

--- LS/libmx/src/memory/mx_memmove.c
+++ LS/libmx/src/memory/mx_memmove.c
@@ -2,10 +2,19 @@
 
 void *mx_memmove(void *dst, const void *src, size_t len) {
     if (len > 0) {
-        uint8_t temp[len];
+        if (len > 1024 * 1024) {
+            uint8_t *temp = malloc(len);
 
-        mx_memcpy(temp, src, len);
-        mx_memcpy(dst, temp, len);
+            mx_memcpy(temp, src, len);
+            mx_memcpy(dst, temp, len);
+            free(temp);
+        }
+        else {
+            uint8_t temp[len];
+
+            mx_memcpy(temp, src, len);
+            mx_memcpy(dst, temp, len);
+        }
     }
     return dst;
 }
--- LS/libmx/src/memory/mx_memset.c
+++ LS/libmx/src/memory/mx_memset.c
@@ -1,9 +1,7 @@
 #include "libmx.h"
 
 void *mx_memset(void *b, int c, size_t len) {
-    uint8_t *ptr = b;
-
-    for (size_t i = 0; i < len; ++i)
-        ptr[i] = c;
+    for (uint8_t *ptr = b; len; --len, ++ptr)
+        *ptr = c;
     return b;
 }
