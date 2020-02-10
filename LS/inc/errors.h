ace16142c830dea02249342c28769c0225db519d
Alex Master <alex.macromaster@outlook.com>
2020-02-10 03:20:57 +0200

    headers manage

--- LS/inc/errors.h
+++ LS/inc/errors.h
@@ -1,2 +1,7 @@
 #pragma once
 #include "uls.h"
+
+void mx_error_nodir(char *argum);
+void mx_error_usage();
+void mx_nelegal(char flag);
+
--- LS/inc/uls.h
+++ LS/inc/uls.h
@@ -17,6 +17,3 @@
 
 void mx_parse(int argnum, char **argum);
 
-void mx_error_nodir(char *argum);
-void mx_error_usage();
-void mx_nelegal(char flag);
