416be92115f54e93319114a066f5fd576441bf9f
Anton Balabin <abalabin@e2r1p1.unit.ua>
2020-02-14 12:55:38 +0200

    Maded base for x flag

--- LS/src/mx_main.c
+++ LS/src/mx_main.c
@@ -15,9 +15,10 @@ typedef struct s_data {
 } t_data;
 
 typedef struct s_offsets {
-    int longest_word;
+    size_t longest_word;
     int wsize;
     int cnum;
+    int cursize;
 } t_offsets;
 
 void get_wsize(t_offsets *offsets) {
@@ -26,6 +27,16 @@ void get_wsize(t_offsets *offsets) {
     ioctl(1, TIOCGSIZE, &ts);
     offsets->cnum = ts.ts_cols;
     offsets->wsize = ts.ts_xxx;
+}
+
+void get_lword(t_vector *v, t_offsets *off) {
+    off->longest_word = 0;
+
+    for (size_t i = 0; i < v->size; ++i) {
+        char *name = (*(t_data **)mx_at(v, i))->dr->d_name;
+        if (mx_strlen(name) > off->longest_word)
+            off->longest_word = mx_strlen(name);
+    }
 }
 
 t_vector *get_files(char *direct) {
@@ -40,31 +51,35 @@ t_vector *get_files(char *direct) {
             dt = (t_data *)malloc(sizeof(t_data));
         }
     }
-    closedir(dir);
-    
     return file_vect;
 }
+
+// int cmp(const void *a, const void *b) {
+//     return mx_strcmp((*(t_data **)a)->dr->d_name, (*(t_data **)b)->dr->d_name) > 0;
+// }s
 
 int main(int argc, char **argv) {
     if (argc) {}
     t_vector *v = get_files(argv[1]);
     t_offsets *offsets = malloc(sizeof(t_offsets));
     get_wsize(offsets);
+    get_lword(v, offsets);
+    offsets->cursize = 0;
 
     for (size_t i = 0; i < v->size; ++i) {
         t_data **dt = mx_at(v, i);
         
-        printf("%s\t", (*dt)->dr->d_name);
+        printf("%s", (*dt)->dr->d_name);
     }
 }
 
 
 // старое
 
-int main(int argc, char **argv) {
-    // Аргументы закинуть в arg_dir посортировать и отправить на парсинг
-    // Там если аргумент не папка он будет из вектора убиратся
-    mx_parse(argc, argv); // должно быть 3 параметра 1 структура куда всё вбивать
-    // Здесь чекнуть на то что папка открывается если нет то говорим permission denied
-    // Здесь начинается сама функция юлса скорее всего
-}
+// int main(int argc, char **argv) {
+//     // Аргументы закинуть в arg_dir посортировать и отправить на парсинг
+//     // Там если аргумент не папка он будет из вектора убиратся
+//     mx_parse(argc, argv); // должно быть 3 параметра 1 структура куда всё вбивать
+//     // Здесь чекнуть на то что папка открывается если нет то говорим permission denied
+//     // Здесь начинается сама функция юлса скорее всего
+// }
