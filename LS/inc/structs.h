177e2b62e4f30393915ef3cce541a6ff6a797d0f
Alex Master <alex.macromaster@outlook.com>
2020-02-17 00:54:21 +0200

    new fields

--- LS/inc/structs.h
+++ LS/inc/structs.h
@@ -5,10 +5,10 @@
 // ACCESS      LINKS    LOGIN     GROUP  SIZE   TIME        ent->d_name
 
 typedef struct s_file {
-    uint64_t inode;
-    uint64_t blocksize;
+    char *inode;
+    char *blocksize;
     char *access;
-    uint64_t links;
+    char *links;
     char *user;
     char *grp;
     char *flags;
@@ -35,25 +35,28 @@ typedef struct s_print t_print;
 typedef struct s_info t_info;
 
 struct s_info {
-    bool output_dst;        // 0 - terminal | 1 - file
+    bool output_dst;        // 0 - terminal | 1 - file or other process
     t_file *files;          // pointer to colection of files in next var
     t_vector array;         // struct for manage array of files
     t_time_type time_type;  // data/time type for -[tlgno]
 
     struct s_print {
-        void (*inode)(t_info *info);        // print.inode
-        void (*bsize)(t_info *info);        // print.bsize
-        void (*access)(t_info *info);       // print.access
-        void (*user)(t_info *info);         // print.user
-        void (*grp)(t_info *info);          // print.grp
-        void (*flags)(t_info *info);        // print.flags
-        void (*size)(t_info *info);         // print.size
-        void (*time)(t_info *info);         // print.time
-        void (*name)(t_info *info);         // print.name
-        void (*name_add)(t_info *info);     // print.name_add
-        void (*arrow)(t_info *info);        // print.arrow
+        void (*inode)(t_info *info);        // print.inode()
+        void (*bsize)(t_info *info);        // print.bsize()
+        void (*access)(t_info *info);       // print.access()
+        void (*user)(t_info *info);         // print.user()
+        void (*grp)(t_info *info);          // print.grp()
+        void (*flags)(t_info *info);        // print.flags()
+        void (*size)(t_info *info);         // print.size()
+        void (*time)(t_info *info);         // print.time()
+        void (*name)(t_info *info);         // print.name()
+        void (*name_add)(t_info *info);     // print.name_add()
+        void (*arrow)(t_info *info);        // print.arrow()
+        void (*access_add)(t_info *info);   // print.access_add()
+        void (*acl)(t_info *info);          // print.acl()
     } print;
 
     void (*out)(t_info *info);  // determines type of printing
+    void (*read)(t_info *info); // filter of hiden files
     void (*sort)(t_info *info); // sort function for files
 };
