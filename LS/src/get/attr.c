526ddcd37f63bc31136068a4926a0e42234d343f
Anton Balabin <abalabin@e2r1p1.unit.ua>
2020-03-02 14:58:46 +0200

    fix audit

--- LS/src/get/attr.c
+++ LS/src/get/attr.c
@@ -3,8 +3,8 @@
 void mx_get_acl(t_dir *dir, t_file *file, struct stat *st) {
     char buff[512];
     char *path = mx_strjoin(dir->name, "/");
-    mx_strcat(path, file->fields.name);
 
+    mx_strcat(path, file->fields.name);
     listxattr(path, buff, 512, XATTR_SHOWCOMPRESSION);
     file->fields.attr = mx_memdup(buff, 512);
 }
