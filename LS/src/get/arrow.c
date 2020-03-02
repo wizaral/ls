c04ef03ff8515651a797f8f6d88c1549a20bfc5a
Anton Balabin <abalabin@e2r1p1.unit.ua>
2020-03-02 14:54:21 +0200

    path in attr and arrow

--- LS/src/get/arrow.c
+++ LS/src/get/arrow.c
@@ -2,6 +2,8 @@
 
 void mx_get_acl(t_dir *dir, t_file *file, struct stat *st) {
     char buff[512];
+    char *path = mx_strjoin(dir->name, "/");
+    mx_strcat(path, file->fields.name);
 
     readlink(path, buff, 512);
     file->fields.arrow = mx_strdup(buff);
--- LS/src/get/attr.c
+++ LS/src/get/attr.c
@@ -2,7 +2,9 @@
 
 void mx_get_acl(t_dir *dir, t_file *file, struct stat *st) {
     char buff[512];
+    char *path = mx_strjoin(dir->name, "/");
+    mx_strcat(path, file->fields.name);
 
-    listxattr(path?, buff, 512, XATTR_SHOWCOMPRESSION);
+    listxattr(path, buff, 512, XATTR_SHOWCOMPRESSION);
     file->fields.attr = mx_memdup(buff, 512);
 }
