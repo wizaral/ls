f5bae8bd885edf1948b347698f8d28b1ed746045
Anton Balabin <abalabin@e2r1p1.unit.ua>
2020-03-02 11:23:17 +0200

    suffix(F flag, but how to only p?)

--- LS/src/get/suffix.c
+++ LS/src/get/suffix.c
@@ -1,7 +1,16 @@
 #include "uls.h"
 
 void mx_get_suffix(t_dir *d, t_file *f, struct stat *s) {
-    ++d;
-    ++f;
-    ++s;
+    if (MX_ISDIR(s->st_mode))
+        f->fields.suffix = '/';
+    else if (s->st_mode & MX_IXUSR)
+        f->fields.suffix = '*';
+    else if (MX_ISLNK(s->st_mode))
+        f->fields.suffix = '@';
+    else if (MX_ISSOCK(s->st_mode))
+        f->fields.suffix = '=';
+    else if (MX_ISWHT(s->st_mode))
+        f->fields.suffix = '%';
+    else if (MX_ISFIFO(s->st_mode))
+        f->fields.suffix = '|';
 }
