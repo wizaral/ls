875be7dd06b862b982d697a0fe62a187d2889367
Anton Balabin <abalabin@e2r1p1.unit.ua>
2020-03-03 11:58:05 +0200

    acl (commented func from my testing)

--- LS/src/get/acl.c
+++ LS/src/get/acl.c
@@ -3,3 +3,27 @@
 // void mx_get_acl(t_dir *dir, t_file *file, t_stat *st) {
 //     advanced_perm(dir, file, st);
 // }
+
+                                        // то что снизу писал в отдельном мейне
+                                        // поэтому без прототипа и тд
+                                        // короче я ебу как это дописывать пока
+    // acl_t acl;
+    // char **acl_text;
+    // char **splited;
+    
+    // acl = acl_get_file(av[1], ACL_TYPE_EXTENDED);
+    // acl_text = mx_strsplit(acl_to_text(acl, NULL), '\n');
+    
+    // for (int i = 1; acl_text[i]; ++i) {
+    //     splited = mx_strsplit(acl_text[i], ':');
+    //     for (int j = 0; splited[j]; ++j) {
+    //         if (j == 0)   
+    //             printf(" %d: %s:", i - 1, splited[j]);
+    //         else if (j == 1 || j == 3) {}
+    //         else if (splited[j + 1])
+    //             printf("%s ", splited[j]);
+    //         else
+    //             printf("%s\n", splited[j]);
+    //     }
+    // }
+    // acl_free(acl);
