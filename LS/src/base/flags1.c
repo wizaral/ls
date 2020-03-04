369c6a45d6e899d5ea5e52009a78f1f21ea93514
Alex Master <alex.macromaster@outlook.com>
2020-03-04 02:19:30 +0200

    full file parsing

--- LS/src/base/flags1.c
+++ LS/src/base/flags1.c
@@ -1,42 +1,34 @@
 #include "uls.h"
 
-static inline void check_read_type(t_info *info, char flag) {
+static inline void check_read(t_info *info, char flag) {
     if (info->read != mx_full)
         info->read = flag == 'A' ? mx_hidden : mx_full;
 }
 
-static inline void check_write_type(t_info *info, char flag) {
-    if (flag == 'l') {
+static inline void check_write(t_info *info, char flag) {
+    if (flag == 'l')
         info->write = mx_write_l;
-        info->get.size = mx_size;
-        info->get.access = mx_access;
-        info->get.suffix = mx_suffix;
-        info->get.arrow = mx_arrow;
-        info->get.grp = mx_group;
-        info->get.time = mx_time;
-        info->get.user = mx_user;
-    }
-    if (flag == '1')
+    else if (flag == '1')
         info->write = mx_write_1;
-    if (flag == 'm')
+    else if (flag == 'm')
         info->write = mx_write_m;
-    if (flag == 'C')
+    else if (flag == 'C')
         info->write = mx_write_C;
-    if (flag == 'x')
+    else if (flag == 'x')
         info->write = mx_write_x;
 }
 
-static inline void check_suffix_type(t_info *info, char flag) {
-    info->write = flag == 'F' ? zaglushka : zaglushka;
+static inline void check_suffix(t_info *info, char flag) {
+    info->write = flag == 'F' ? mx_suffix_all : mx_suffix_dir;
 }
 
-static inline void check_time_type(t_info *info, char flag) {
+static inline void check_time(t_info *info, char flag) {
     if (flag == 'c')
-        info->time_type = change;
+        info->time = change;
     else if (flag == 'U')
-        info->time_type = creation;
+        info->time = creation;
     else if (flag == 'u')
-        info->time_type = last_access;
+        info->time = last_access;
 }
 
 void mx_check_flags(t_info *info, t_vector *flags) {
@@ -45,20 +37,22 @@ void mx_check_flags(t_info *info, t_vector *flags) {
         char flag = flags->arr[i];
 
         if (flag == 'a' || flag == 'A')
-            check_read_type(info, flag);
+            check_read(info, flag);
         else if (MX_WRITE(flag))
-            check_write_type(info, flag);
+            check_write(info, flag);
         else if (flag == 'F' || flag == 'p')
-            check_suffix_type(info, flag);
+            check_suffix(info, flag);
         else if (flag == 'c' || flag == 'U' || flag == 'u')
-            check_time_type(info, flag);
+            check_time(info, flag);
         else if (flag == 'f' || flag == 'S' || flag == 't')
-            mx_check_sort_type(info, flag);
+            mx_check_sort(info, flag);
         else if (flag == 'q' || flag == 'v' || flag == 'w')
-            mx_check_name_type(info, flag);
+            mx_check_name(&info->get, flag);
         else if (MX_LADDS(flag))
-            mx_check_adds_type(info, flag);
+            mx_check_adds(info, &info->get, flag);
         else if (MX_OTHER(flag))
-            mx_check_other_type(info, flag);
+            mx_check_other(info, flag);
+        else
+            printf("WATAFAK is this: \"%c\"!!!\n", flag);   // delete before deploy
     }
 }
--- LS/src/base/flags2.c
+++ LS/src/base/flags2.c
@@ -1,9 +1,11 @@
 #include "uls.h"
 
-mx_check_sort_type(t_info *info, char flag) {
-    if (info->cmp != NULL) {
-        if (flag == 'f')
-            info->cmp = NULL;
+void mx_check_sort(t_info *info, char flag) {
+    if (info->cmp != NULL) {        // мейби даже тут не скомпилит я хз еси шо))0)
+        if (flag == 'f') {
+            info->cmp = NULL;       // may not compile. сверху бахни функу-затычку если шоя потом порешаю
+            info->read = mx_full;
+        }
         else if (flag == 'S')
             info->cmp = mx_compare_size;
         else if (info->cmp != mx_compare_size)
@@ -11,14 +13,55 @@ mx_check_sort_type(t_info *info, char flag) {
     }
 }
 
-mx_check_name_type(t_info *info, char flag) {
-    info->get.name = flag == 'q' ? mx_only_printable : mx_not_printable;
+void mx_check_name(t_get *get, char flag) {
+    get->name = flag == 'q' ? mx_only_printable : mx_not_printable;
 }
 
-mx_check_adds_type(t_info *info, char flag) {
-
+void mx_check_adds(t_info *info, t_get *get, char flag) {
+    if (flag == 'g' || flag == 'n') {
+        info->write = mx_write_l;
+        if (flag == 'n' && get->user != mx_user_skip)
+            get->user = mx_user_id;
+        get->user = mx_user_skip;
+    }
+    else if (flag == 'O')
+        get->flags = mx_file_flags;
+    else if (flag == 'h')
+        get->size = mx_size_h;
+    else if (flag == 'T')
+        get->time = mx_time_full;
+    else if (flag == '@')
+        get->attr = mx_attr;
+    else
+        get->acl = mx_acl;
 }
 
-mx_check_other_type(t_info *info, char flag) {
+void mx_check_other(t_info *info, char flag) {
+    if (flag == 'i')
+        info->get.inode = mx_inode;
+    else if (flag == 's')
+        info->get.bsize = mx_bsize;
+    else if (flag == 'G')
+        info->print_name = info->output_dst ? mx_nocolor : mx_color;
+    else if (flag == 'r')
+        info->foreach = mx_foreach_vector_reverse;
+    else
+        info->recursion = mx_recursion;
+}
 
+void mx_minimize_flags(t_info *info) {
+    if (info->write == mx_write_l) {
+        get->access = mx_access;
+        get->links = mx_links;
+        get->user == mx_dummy ? get->user = mx_user_name : mx_dummy;
+        get->grp = mx_group;
+        get->size == mx_dummy ? get->size = mx_size_b : mx_dummy;
+        get->time == mx_dummy ? get->time = mx_time_short : mx_dummy;
+        get->arrow = mx_arrow;
+    }
+    else {
+        get->access = get->links = get->user = get->grp = mx_dummy;
+        get->flags = get->size = get->time = get->arrow = mx_dummy;
+        get->attr = get->acl = mx_dummy;
+    }
 }
