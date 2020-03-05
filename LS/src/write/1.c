817c2ed858f0699956954e6c169aa76f5b9c2733
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-05 16:02:51 +0200

    done 1 and l

--- LS/src/write/1.c
+++ LS/src/write/1.c
@@ -1,37 +1,31 @@
 #include "uls.h"
 
-// static inline size_t get_len(t_offset *off) {
-//     return off->inode + off->bsize + (off->inode > 0) + (off->bsize > 0);
-// }
+static inline size_t get_len(t_offset *off) {
+    return off->inode + off->bsize + (off->inode > 0) + (off->bsize > 0);
+}
 
-// static void create_str(t_offset *off, char *str,
-//                        t_printable *print, t_lengths *len) {
-//     int pos = 0;
+static void create_str(t_offset *off, char *str,
+                       t_printable *print, t_lengths *len) {
+    int pos = 0;
 
-//     if (print->inode) {
-//         mx_memcpy(str + off->inode - len->inode, print->inode, len->inode);
-//         pos += off->inode + (off->inode > 0);
-//     }
-//     if (print->bsize) {
-//         mx_memcpy(str + pos + off->bsize - len->bsize, print->bsize, len->bsize);
-//         pos += off->bsize + (off->bsize > 0);
-//     }
-// }
-
-// static void print(t_info *info, t_dir *dir, t_file *file) {
-//     size_t len = get_len(&dir->off);
-//     char str[len];
-
-//     mx_memset(str, ' ', len);
-//     create_str(&dir->off, str, &file->fields, &file->lengths);
-//     mx_printstrlen(str, len, 1);
-//     info->print_name(file);
-//     mx_printstrlen(&file->fields.suffix, file->lengths.suffix, 1);
-//     mx_printchar('\n', 1);
-// }
+    mx_memcpy(str + off->inode - len->inode, print->inode, len->inode);
+    pos += off->inode + (off->inode > 0);
+    mx_memcpy(str + pos + off->bsize - len->bsize, print->bsize, len->bsize);
+    pos += off->bsize + (off->bsize > 0);
+}
 
 void mx_write_1(t_info *info, t_dir *dir) {
-    // info->foreach(info, dir, print);
-    ++info;
-    ++dir;
+    size_t len = get_len(&dir->off);
+    char str[len];
+    t_file *file;
+
+    for (size_t i = 0; i < dir->array.size; ++i) {
+        mx_memset(str, ' ', len);
+        file = mx_at(&dir->array, i);
+        create_str(&dir->off, str, &file->fields, &file->lengths);
+        mx_printstrlen(str, len, 1);
+        info->print_name(file);
+        mx_printstrlen(&file->fields.suffix, file->lengths.suffix, 1);
+        mx_printchar('\n', 1);
+    }
 }
--- LS/src/write/l.c
+++ LS/src/write/l.c
@@ -1,53 +1,51 @@
 #include "uls.h"
 
-// static inline size_t get_len(t_info *info, t_offset *off) {
-//     return off->inode + off->bsize + off->links + off->user
-//         + off->grp + off->flags + off->size + (off->inode > 0)
-//         + (off->bsize > 0) + (off->user > 0) + (off->user > 0)
-//         + (off->flags > 0) + 12 + 1 + 2 + 1
-//         + (info->get.time == mx_time_full ? 21 : 13);
-// }
+static inline size_t get_len(t_info *info, t_offset *off) {
+    return off->inode + off->bsize + off->links + off->user
+        + off->grp + off->flags + off->size + (off->inode > 0)
+        + (off->bsize > 0) + (off->user > 0) + (off->user > 0)
+        + (off->flags > 0) + 12 + 1 + 2 + 1
+        + (info->get.time == mx_time_full ? 21 : 13);
+}
 
-// static void create_str(t_offset *off, char *str,
-//                        t_printable *print, t_lengths *len) {
-//     int pos = 0;
+static void create_str(t_offset *off, char *str,
+                       t_printable *print, t_lengths *len) {
+    int pos = 0;
 
-//     mx_memcpy(str + off->inode - len->inode, print->inode, len->inode);
-//     pos += off->inode + (off->inode > 0);
-//     mx_memcpy(str + pos + off->bsize - len->bsize, print->bsize, len->bsize);
-//     pos += off->bsize + (off->bsize > 0);
-//     mx_memcpy(str + pos, print->access, 12);
-//     pos += 12;
-//     mx_memcpy(str + pos + off->links - len->links, print->links, len->links);
-//     pos += off->links + 1;
-//     mx_memcpy(str + pos + off->user - len->user, print->user, len->user);
-//     pos += off->user + (off->user > 0) + (off->user > 0);
-//     mx_memcpy(str + pos + off->grp - len->grp, print->grp, len->grp);
-//     pos += off->grp + 2;
-//     mx_memcpy(str + pos + off->flags - len->flags, print->flags, len->flags);
-//     pos += off->flags + (off->flags > 0);
-//     mx_memcpy(str + pos + off->size - len->size, print->size, len->size);
-//     pos += off->size + 1;
-//     mx_memcpy(str + pos, print->time, len->time);
-// }
-
-// static void print(t_info *info, t_dir *dir, t_file *file) {
-//     size_t len = get_len(info, &dir->off);
-//     char str[len];
-
-//     mx_memset(str, ' ', len);
-//     create_str(&dir->off, str, &file->fields, &file->lengths);
-//     mx_printstrlen(str, len, 1);
-//     info->print_name(file);
-//     mx_printstrlen(&file->fields.suffix, file->lengths.suffix, 1);
-//     mx_printstrlen(file->fields.arrow, file->lengths.arrow, 1);
-//     mx_printchar('\n', 1);
-//     mx_printstrlen(file->fields.attr, file->lengths.attr, 1);
-//     mx_printstrlen(file->fields.acl, file->lengths.acl, 1);
-// }
+    mx_memcpy(str + off->inode - len->inode, print->inode, len->inode);
+    pos += off->inode + (off->inode > 0);
+    mx_memcpy(str + pos + off->bsize - len->bsize, print->bsize, len->bsize);
+    pos += off->bsize + (off->bsize > 0);
+    mx_memcpy(str + pos, print->access, 12);
+    pos += 12;
+    mx_memcpy(str + pos + off->links - len->links, print->links, len->links);
+    pos += off->links + 1;
+    mx_memcpy(str + pos + off->user - len->user, print->user, len->user);
+    pos += off->user + (off->user > 0) + (off->user > 0);
+    mx_memcpy(str + pos + off->grp - len->grp, print->grp, len->grp);
+    pos += off->grp + 2;
+    mx_memcpy(str + pos + off->flags - len->flags, print->flags, len->flags);
+    pos += off->flags + (off->flags > 0);
+    mx_memcpy(str + pos + off->size - len->size, print->size, len->size);
+    pos += off->size + 1;
+    mx_memcpy(str + pos, print->time, len->time);
+}
 
 void mx_write_l(t_info *info, t_dir *dir) {
-    // info->foreach(info, dir, print);
-    ++info;
-    ++dir;
+    size_t len = get_len(info, &dir->off);
+    char str[len];
+    t_file *file;
+
+    for (size_t i = 0; i < dir->array.size; ++i) {
+        mx_memset(str, ' ', len);
+        file = mx_at(&dir->array, i);
+        create_str(&dir->off, str, &file->fields, &file->lengths);
+        mx_printstrlen(str, len, 1);
+        info->print_name(file);
+        mx_printstrlen(&file->fields.suffix, file->lengths.suffix, 1);
+        mx_printstrlen(file->fields.arrow, file->lengths.arrow, 1);
+        mx_printchar('\n', 1);
+        mx_printstrlen(file->fields.attr, file->lengths.attr, 1);
+        mx_printstrlen(file->fields.acl, file->lengths.acl, 1);
+    }
 }
