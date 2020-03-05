b7e2f8c894d3877e711e69f791d7d1a6bcd35891
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-05 21:52:23 +0200

    fix sorts 100%

--- LS/src/sort/time.c
+++ LS/src/sort/time.c
@@ -1,12 +1,12 @@
 #include "uls.h"
 
 int mx_compare_time(const void *a, const void *b) {
-    const t_file *f1 = a;
-    const t_file *f2 = b;
+    const t_file *f1 = b;
+    const t_file *f2 = a;
 
     if (f1->time.tv_sec != f2->time.tv_sec)
         return f1->time.tv_sec - f2->time.tv_sec;
     if (f1->time.tv_nsec != f2->time.tv_nsec)
         return f1->time.tv_nsec - f2->time.tv_nsec;
-    return mx_strcmp(f1->fields.name, f2->fields.name);
+    return mx_strcmp(f2->fields.name, f1->fields.name);
 }
