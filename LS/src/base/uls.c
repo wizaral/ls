1fb84ba36b7af142af48e099732ca2f80884d0cb
Alex Master <alex.macromaster@outlook.com>
2020-03-08 01:03:12 +0200

    print total

--- LS/src/base/uls.c
+++ LS/src/base/uls.c
@@ -1,11 +1,6 @@
 #include "uls.h"
 
 // should placed in utils
-// static bool check_block_char(char *name) {
-//     return false;
-// }
-
-// should placed in utils
 // static void free_dir(t_dir *dir) {
 // }
 
@@ -15,29 +10,29 @@ static void get_info(t_info *info, t_dir *dir, t_dirent *file) {
     void (**func)(t_info *, t_dir *, t_file *, t_stat *) =
         (void (**)(t_info *, t_dir *, t_file *, t_stat *))&info->get;
 
-    dir->filename = mx_get_path_name(dir->name, dir->len,
-                                file->d_name, file->d_namlen);
-
+    dir->filename = mx_get_path_name(dir->name, dir->len, file->d_name, file->d_namlen);
     lstat(dir->filename, &st);
+    dir->total += st.st_blocks;
     file_info.size = st.st_size;
     file_info.mode = st.st_mode;
     mx_memcpy(&file_info.time, &st.st_atimespec + info->time_type, sizeof(t_timespec));
 
     for (int i = 0; i < 14; ++i)
-        func[i](info, dir, &file_info, &st);    // check about NULLs
+        func[i](info, dir, &file_info, &st);
     mx_push_backward(&dir->array, &file_info);
     mx_strdel(&dir->filename);
 }
 
 void mx_uls(t_info *info, t_dir *dir) {
     t_dirent *(*read)(DIR *) = info->read;
-    dir->file = read(dir->dir);
 
-    // dir->has_bc = check_block_char(dir->name);
+    dir->file = read(dir->dir);
+    dir->has_bc = mx_check_block_char(dir->name);
     for (DIR *pdir = dir->dir; dir->file; dir->file = read(pdir))
         get_info(info, dir, dir->file);
 
     mx_sort(dir->array.arr, dir->array.size, dir->array.bytes, info->cmp);
+    info->print_total(info, dir);
     info->write(info, dir);
     // free_dir();
 }
@@ -46,10 +41,12 @@ void mx_recursion(t_info *info, t_dir *dir) {
     t_dirent *(*read)(DIR *) = info->read;
 
     dir->file = read(dir->dir);
-    // dir->has_bc = check_block_char();
+    dir->has_bc = mx_check_block_char(dir->name);
     for (DIR *pdir = dir->dir; dir->file; dir->file = read(pdir))
         get_info(info, dir, dir->file);
+
     mx_sort(dir->array.arr, dir->array.size, dir->array.bytes, info->cmp);
+    info->print_total(info, dir);
     info->write(info, dir);
 
     t_file *end = (t_file *)(dir->array.arr + dir->array.size * dir->array.bytes);
