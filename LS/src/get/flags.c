702cbab907c4fde657325009f6c509e0a18f6a40
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-08 16:40:39 +0200

    more info for O flag

--- LS/src/get/flags.c
+++ LS/src/get/flags.c
@@ -7,17 +7,15 @@ void mx_file_flags(t_info *info, t_dir *dir, t_file *file, t_stat *st) {
     ++st;
 }
 
-/*
-#define SAPPEND(s)                        \
-    {                                     \
-        if (prefix != NULL)               \
-            (void)strcat(string, prefix); \
-        (void)strcat(string, s);          \
-        prefix = ",";                     \
-    }
-*/
+// #define SAPPEND(s)                        \
+//     {                                     \
+//         if (prefix != NULL)               \
+//             strcat(string, prefix);       \
+//         strcat(string, s);                \
+//         prefix = ",";                     \
+//     }
 
-// char *flags_to_string(flags, def) u_long flags; char *def; {
+// char *flags_to_string(flags, def) {
 //     static char string[128];
 //     char *prefix;
 
@@ -38,4 +36,53 @@ void mx_file_flags(t_info *info, t_dir *dir, t_file *file, t_stat *st) {
 //     if (flags & SF_IMMUTABLE)
 //         SAPPEND("schg");
 //     return (prefix == NULL && def != NULL ? def : string);
+// }
+
+// static void get_flags(t_const *cnst, char *str) {
+//     char *tmp = NULL;
+
+//     tmp = cnst->flags;
+//     if (cnst->flags != NULL) {
+//         cnst->flags = mx_strjoin(tmp, ",");
+//         free(tmp);
+//     }
+//     tmp = cnst->flags;
+//     cnst->flags = mx_strjoin(tmp, str);
+//     free(tmp);
+// }
+
+// static void root_flags(t_const *cnst, struct stat st) {
+//     if (st.st_flags & SF_NOUNLINK) // sunlnk
+//         get_flags(cnst, "sunlnk");
+//     if (st.st_flags & SF_RESTRICTED) // restricted
+//         get_flags(cnst, "restricted");
+//     if (st.st_flags & UF_HIDDEN) // hidden
+//         get_flags(cnst, "hidden");
+//     if (st.st_flags & UF_COMPRESSED) // compressed 
+//         get_flags(cnst, "compressed");
+// }
+
+// static void user_flags(t_const *cnst, struct stat st) {
+//     if (st.st_flags & UF_APPEND)
+//         get_flags(cnst, "uappnd");
+//     if (st.st_flags & UF_IMMUTABLE)
+//         get_flags(cnst, "uchg");
+//     if (st.st_flags & UF_NODUMP)
+//         get_flags(cnst, "nodump");
+//     if (st.st_flags & UF_OPAQUE)
+//         get_flags(cnst, "opaque");
+//     if (st.st_flags & SF_APPEND)
+//         get_flags(cnst, "sappnd");
+//     if (st.st_flags & SF_ARCHIVED)
+//         get_flags(cnst, "arch");
+//     if (st.st_flags & SF_IMMUTABLE)
+//         get_flags(cnst, "schg");
+// }
+
+// void mx_get_flags_for_file(t_const *cnst, struct stat st) {
+//     cnst->flags = NULL;
+//     user_flags(cnst, st);
+//     root_flags(cnst, st);
+//     if (cnst->flags == NULL)
+//         cnst->flags = "-";
 // }
