3ccbdaacf098e2b4505c56674ba41d846633834d
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-08 16:35:28 +0200

    update usage and colors description

--- LS/inc/defines.h
+++ LS/inc/defines.h
@@ -1,21 +1,20 @@
 #pragma once
 #include "uls.h"
 
-#define MX_FLAGS "ABCFGORSTU@abcefghilmnpqrstuvwx1"
+#define MX_FLAGS "ACFGORSTU@acefghilmnpqrstuvwx1"
 #define MX_USAGE "usage: uls [-"MX_FLAGS"] [file ...]"
-#define MX_USAGE_OLD "usage: uls [-ABCFGORSTU@abcefghilmnpqrstuvwx1] [file ...]"
 
 #define MX_DEFAULT "\033[0m"            // NO COLOR
-#define MX_EXECUTE "\033[0;31m"         // --x------ -----x--- --------x
-#define MX_SOCKET "\033[0;32m"
-#define MX_PIPE "\033[0;33m"
-#define MX_DIR "\033[0;34m"
-#define MX_LINK "\033[0;35m"
-#define MX_CHAR "\033[34;43m"
-#define MX_BLOCK "\033[34;46m"
-#define MX_UIDBIT "\033[30;41m"         // --s------
-#define MX_GIDBIT "\033[30;46m"         // -----s---
-#define MX_STICKYBIT "\033[30;42m"      // -------wT -------wt
-#define MX_NOTSTICKYBIT "\033[30;43m"   // -------w-
+#define MX_EXECUTE "\033[0;31m"         // ---x--x--x
+#define MX_SOCKET "\033[0;32m"          // s---------
+#define MX_PIPE "\033[0;33m"            // p---------
+#define MX_DIR "\033[0;34m"             // d---------
+#define MX_LINK "\033[0;35m"            // l---------
+#define MX_CHAR "\033[34;43m"           // c---------
+#define MX_BLOCK "\033[34;46m"          // b---------
+#define MX_UIDBIT "\033[30;41m"         // ---s------
+#define MX_GIDBIT "\033[30;46m"         // ------s---
+#define MX_STICKYBIT "\033[30;42m"      // --------wT --------wt
+#define MX_NOTSTICKYBIT "\033[30;43m"   // --------w-
 
 #define MX_HALF_YEAR 15768000
