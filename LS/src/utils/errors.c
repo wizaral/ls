9a2cb1d982a8d11b7a4b3a57d07683a4d00676d1
Alex Master <alex.macromaster@outlook.com>
2020-03-10 01:35:54 +0200

    replace file

---
+++ LS/src/utils/errors.c
@@ -0,0 +1,14 @@
+#include "uls.h"
+
+void mx_wrong_flag(char flag) {
+    mx_printstrlen("uls: illegal option -- ", 23, 2);
+    mx_printchar(flag, 2);
+    mx_printstrlen("\n" MX_USAGE "\n", mx_strlen("\n" MX_USAGE "\n"), 2);
+    exit(1);
+}
+
+void mx_wrong_argv(t_info *info, const char *av) {
+    info->return_val = 1;
+    mx_printstrlen("uls: ", 5, 2);
+    perror(av);
+}
