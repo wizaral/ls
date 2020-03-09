603a31c5cd0c4e45b60b0ad4e7439adad232c85f
Alex Master <alex.macromaster@outlook.com>
2020-03-10 01:40:21 +0200

    new macro for . and ..

--- LS/inc/macro.h
+++ LS/inc/macro.h
@@ -4,10 +4,10 @@
 #define MX_ISBLK(x)    (((x) & S_IFMT) == S_IFBLK)     // block special
 #define MX_ISCHR(x)    (((x) & S_IFMT) == S_IFCHR)     // char special
 #define MX_ISDIR(x)    (((x) & S_IFMT) == S_IFDIR)     // directory
-#define MX_ISFIFO(x)   (((x) & S_IFMT) == S_IFIFO)     // fifo or socket
+#define MX_ISFIF(x)    (((x) & S_IFMT) == S_IFIFO)     // fifo or socket
 #define MX_ISREG(x)    (((x) & S_IFMT) == S_IFREG)     // regular file
 #define MX_ISLNK(x)    (((x) & S_IFMT) == S_IFLNK)     // symbolic link
-#define MX_ISSOCK(x)   (((x) & S_IFMT) == S_IFSOCK)    // socket
+#define MX_ISSCK(x)    (((x) & S_IFMT) == S_IFSOCK)    // socket
 #if !defined(_POSIX_C_SOURCE) || defined(_DARWIN_C_SOURCE)
 #define MX_ISWHT(x)    (((x) & S_IFMT) == S_IFWHT)     // whiteout
 #endif
@@ -23,3 +23,7 @@
 #define MX_WRITE(x) mx_strchr("l1mCx", x)
 #define MX_LADDS(x) mx_strchr("ghnOT@e", x)
 #define MX_OTHER(x) mx_strchr("isGRr", x)
+
+#define MX_DOT1(x) ((x)[0] == '.' && (x)[1] == '\0')
+#define MX_DOT2(x) ((x)[0] == '.' && (x)[1] == '.' && (x)[2] == '\0')
+#define MX_DOTS(x) (MX_DOT1(x) || MX_DOT2(x))
