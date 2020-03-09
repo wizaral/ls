7a9bc946f865e7d819ebd51bfd0979ddd248463b
Alex Master <alex.macromaster@outlook.com>
2020-03-10 01:42:46 +0200

    delete parameter and rudiment funcs. add errors

--- LS/inc/uls.h
+++ LS/inc/uls.h
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/xattr.h>
+#include <time.h>
 
 #include "defines.h"
 #include "libmx.h"
@@ -17,7 +18,8 @@
 
 #include <sys/acl.h>
 
-void mx_wrong(char flag);
+void mx_wrong_flag(char flag);
+void mx_wrong_argv(t_info *info, const char *av);
 void mx_parse(t_info *info, int ac, char **av);
 void mx_check_flags(t_info *info, t_vector *flags);
 void mx_check_adds(t_info *info, t_get *get, char flag);
@@ -37,10 +39,6 @@ size_t mx_get_inode_bsize_len(t_offset *off);
 void mx_make_inode_bsize(t_offset *off, char *str, t_printable *print, t_lengths *len);
 char *mx_get_path_name(char *path, size_t plen, char *name, size_t nlen);
 uint16_t mx_winsize(t_info *info);
-
-size_t mx_octal_off(const char *name, int len);
-size_t mx_octal_len(const char *name, int len);
-size_t mx_escape_len(const char *name, int len);
 
 t_dirent *mx_full(DIR *dir);        // -a
 t_dirent *mx_hidden(DIR *dir);      // -A
@@ -77,35 +75,35 @@ int mx_compare_argv_r(const void *a, const void *b);  // -r
  * they can't update any structs of global state or current directory
 */
 
-void mx_inode(t_info *info, t_dir *dir, t_file *file, t_stat *st);              // -i
-void mx_bsize(t_info *info, t_dir *dir, t_file *file, t_stat *st);              // -s
+void mx_inode(t_dir *dir, t_file *file, t_stat *st);            // -i
+void mx_bsize(t_dir *dir, t_file *file, t_stat *st);            // -s
 
-void mx_access(t_info *info, t_dir *dir, t_file *file, t_stat *st);             // -l
-void mx_links(t_info *info, t_dir *dir, t_file *file, t_stat *st);              // -l
+void mx_access(t_dir *dir, t_file *file, t_stat *st);           // -l
+void mx_links(t_dir *dir, t_file *file, t_stat *st);            // -l
 
-void mx_user_id(t_info *info, t_dir *dir, t_file *file, t_stat *st);            // -n(l)
-void mx_user_name(t_info *info, t_dir *dir, t_file *file, t_stat *st);          // -l
-void mx_user_skip(t_info *info, t_dir *dir, t_file *file, t_stat *st);          // -g(l)
-void mx_group(t_info *info, t_dir *dir, t_file *file, t_stat *st);              // -l
+void mx_user_id(t_dir *dir, t_file *file, t_stat *st);          // -n(l)
+void mx_user_name(t_dir *dir, t_file *file, t_stat *st);        // -l
+void mx_user_skip(t_dir *dir, t_file *file, t_stat *st);        // -g(l)
 
-void mx_file_flags(t_info *info, t_dir *dir, t_file *file, t_stat *st);         // -O -l
+void mx_grp_name(t_dir *dir, t_file *file, t_stat *st);         // -l
+void mx_grp_skip(t_dir *dir, t_file *file, t_stat *st);         // -o(l)
 
-void mx_size_h(t_info *info, t_dir *dir, t_file *file, t_stat *st);             // -h -l
-void mx_size_b(t_info *info, t_dir *dir, t_file *file, t_stat *st);             // -l
+void mx_file_flags(t_dir *dir, t_file *file, t_stat *st);       // -O -l
 
-void mx_time_short(t_info *info, t_dir *dir, t_file *file, t_stat *st);         // -l
-void mx_time_full(t_info *info, t_dir *dir, t_file *file, t_stat *st);          // -T -l
+void mx_size_h(t_dir *dir, t_file *file, t_stat *st);           // -h -l
+void mx_size_b(t_dir *dir, t_file *file, t_stat *st);           // -l
 
-void mx_not_printable(t_info *info, t_dir *dir, t_file *file, t_stat *st);      // -v -w
-void mx_only_printable(t_info *info, t_dir *dir, t_file *file, t_stat *st);     // -q
-void mx_octal_printable(t_info *info, t_dir *dir, t_file *file, t_stat *st);    // -B
-void mx_escape_printable(t_info *info, t_dir *dir, t_file *file, t_stat *st);   // -b
+void mx_time_short(t_dir *dir, t_file *file, t_stat *st);       // -l
+void mx_time_full(t_dir *dir, t_file *file, t_stat *st);        // -T -l
 
-void mx_suffix_all(t_info *info, t_dir *dir, t_file *file, t_stat *st);         // -F
-void mx_suffix_dir(t_info *info, t_dir *dir, t_file *file, t_stat *st);         // -p
+void mx_not_printable(t_dir *dir, t_file *file, t_stat *st);    // -v -w
+void mx_only_printable(t_dir *dir, t_file *file, t_stat *st);   // -q
 
-void mx_arrow(t_info *info, t_dir *dir, t_file *file, t_stat *st);              // -l
-void mx_attr(t_info *info, t_dir *dir, t_file *file, t_stat *st);               // -@
-void mx_acl(t_info *info, t_dir *dir, t_file *file, t_stat *st);                // -e
+void mx_suffix_all(t_dir *dir, t_file *file, t_stat *st);       // -F
+void mx_suffix_dir(t_dir *dir, t_file *file, t_stat *st);       // -p
 
-void mx_dummy(t_info *info, t_dir *dir, t_file *file, t_stat *st);             // do nothing
+void mx_arrow(t_dir *dir, t_file *file, t_stat *st);            // -l
+void mx_attr(t_dir *dir, t_file *file, t_stat *st);             // -@
+void mx_acl(t_dir *dir, t_file *file, t_stat *st);              // -e
+
+void mx_dummy(t_dir *dir, t_file *file, t_stat *st);
