d78bcc03e0aea8a21f9ea21eb9cf5814b274e763
Alex Master <alex.macromaster@outlook.com>
2020-03-10 01:40:04 +0200

    rename defines

--- LS/inc/defines.h
+++ LS/inc/defines.h
@@ -1,20 +1,20 @@
 #pragma once
 #include "uls.h"
 
-#define MX_FLAGS "ACFGORSTU@acefghilmnpqrstuvwx1"
+#define MX_FLAGS "ACFGORSTU@acdefghilmnopqrstuvwx1"
 #define MX_USAGE "usage: uls [-"MX_FLAGS"] [file ...]"
 
-#define MX_DEFAULT "\033[0m"            // NO COLOR
-#define MX_EXECUTE "\033[0;31m"         // ---x--x--x
-#define MX_SOCKET "\033[0;32m"          // s---------
-#define MX_PIPE "\033[0;33m"            // p---------
-#define MX_DIR "\033[0;34m"             // d---------
-#define MX_LINK "\033[0;35m"            // l---------
-#define MX_CHAR "\033[34;43m"           // c---------
-#define MX_BLOCK "\033[34;46m"          // b---------
-#define MX_UIDBIT "\033[30;41m"         // ---s------
-#define MX_GIDBIT "\033[30;46m"         // ------s---
-#define MX_STICKYBIT "\033[30;42m"      // --------wT --------wt
-#define MX_NOTSTICKYBIT "\033[30;43m"   // --------w-
+#define MX_DEF "\033[0m"        // ----------       // default
+#define MX_EXE "\033[0;31m"     // ---x--x--x       // execute
+#define MX_SCK "\033[0;32m"     // s---------       // socket
+#define MX_FIF "\033[0;33m"     // p---------       // fifo
+#define MX_DIR "\033[0;34m"     // d---------       // directory
+#define MX_LNK "\033[0;35m"     // l---------       // link
+#define MX_CHR "\033[34;43m"    // c---------       // char
+#define MX_BLK "\033[34;46m"    // b---------       // block
+#define MX_UIB "\033[30;41m"    // ---s------       // user id bit
+#define MX_GIB "\033[30;46m"    // ------s---       // group id bit
+#define MX_SKB "\033[30;42m"    // --------w[Tt]    // sticky bit
+#define MX_NSB "\033[30;43m"    // --------w-       // not sticky bit
 
 #define MX_HALF_YEAR 15768000
--- LS/src/write/name.c
+++ LS/src/write/name.c
@@ -2,20 +2,20 @@
 
 static inline void file(char *access) {
     if (access[3] == 's')
-        mx_printstrlen(MX_UIDBIT, 8, 1);
+        mx_printstrlen(MX_UIB, 8, 1);
     else if (access[6] == 's')
-        mx_printstrlen(MX_GIDBIT, 8, 1);
+        mx_printstrlen(MX_GIB, 8, 1);
     else if (access[3] == 'x' || access[6] == 'x'
         || access[9] == 'x' || access[9] == 't')
-        mx_printstrlen(MX_EXECUTE, 7, 1);
+        mx_printstrlen(MX_EXE, 7, 1);
 }
 
 static inline void dir(char *access) {
     if (access[8] == 'w') {
         if (access[9] == 'T' || access[9] == 't')
-            mx_printstrlen(MX_STICKYBIT, 8, 1);
+            mx_printstrlen(MX_SKB, 8, 1);
         else
-            mx_printstrlen(MX_NOTSTICKYBIT, 8, 1);
+            mx_printstrlen(MX_NSB, 8, 1);
     }
     else
         mx_printstrlen(MX_DIR, 7, 1);
@@ -27,21 +27,21 @@ static inline void set_color(char *access) {
     else if (access[0] == 'd')
         dir(access);
     else if (access[0] == 's')
-        mx_printstrlen(MX_SOCKET, 7, 1);
+        mx_printstrlen(MX_SCK, 7, 1);
     else if (access[0] == 'p')
-        mx_printstrlen(MX_PIPE, 7, 1);
+        mx_printstrlen(MX_FIF, 7, 1);
     else if (access[0] == 'l')
-        mx_printstrlen(MX_LINK, 7, 1);
+        mx_printstrlen(MX_LNK, 7, 1);
     else if (access[0] == 'b')
-        mx_printstrlen(MX_BLOCK, 8, 1);
+        mx_printstrlen(MX_BLK, 8, 1);
     else if (access[0] == 'c')
-        mx_printstrlen(MX_CHAR, 8, 1);
+        mx_printstrlen(MX_CHR, 8, 1);
 }
 
 void mx_color(t_file *file) {
     set_color(file->fields.access);
     mx_printstrlen(file->fields.name, file->lengths.name, 1);
-    mx_printstrlen(MX_DEFAULT, 4, 1);
+    mx_printstrlen(MX_DEF, 4, 1);
 }
 
 void mx_nocolor(t_file *file) {
