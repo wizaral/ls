0fc3643043e544a92e498274f7e997902af9f9e7
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-10 20:12:22 +0200

    fix SEGV in arrow

--- LS/src/get/arrow.c
+++ LS/src/get/arrow.c
@@ -2,15 +2,15 @@
 
 void mx_arrow(t_dir *dir, t_file *file, t_stat *st) {
     if (MX_ISLNK(st->st_mode)) {
-        size_t size = MX_MIN(st->st_size, 1024, int64_t) + 1;
-        char buff[size];
+        size_t size = st->st_size ? MX_MIN(st->st_size, 256, int64_t) : 256;
+        char buff[size + 1];
         ssize_t len = readlink(dir->filename, buff, size);
 
         if (len != -1) {
-            buff[file->size] = '\0';
-            file->lengths.arrow = file->size + 4;
+            buff[len] = '\0';
+            file->lengths.arrow = len + 4;
             file->fields.arrow = (char *)mx_memcpy((char *)mx_memcpy(
-                malloc(file->size + 5), " -> ", 4) + 4, buff, file->size + 1) - 4;
+                malloc(len + 5), " -> ", 4) + 4, buff, len + 1) - 4;
         }
     }
 }
