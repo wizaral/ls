99a7128bf0a4c6df55d767fed23156eccf0a9b48
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-10 17:54:09 +0200

    arrow only for LNKs. fix offset for ->

--- LS/src/get/arrow.c
+++ LS/src/get/arrow.c
@@ -1,14 +1,16 @@
 #include "uls.h"
 
 void mx_arrow(t_dir *dir, t_file *file, t_stat *st) {
-    static char buff[1024];
-    ssize_t len = readlink(dir->filename, buff, 1024);
+    if (MX_ISLNK(st->st_mode)) {
+        size_t size = MX_MIN(st->st_size, 1024, int64_t) + 1;
+        char buff[size];
+        ssize_t len = readlink(dir->filename, buff, size);
 
-    if (len != -1 && ++st) {
-        buff[len] = '\0';
-        file->lengths.arrow = len;
-        file->fields.arrow = (char *)mx_memcpy((char *)mx_memcpy(
-            malloc(len + 5), " -> ", 4) + 4, buff, len + 1) - 4;
-        mx_memset(buff, 0, 1024);
+        if (len != -1) {
+            buff[file->size] = '\0';
+            file->lengths.arrow = file->size + 4;
+            file->fields.arrow = (char *)mx_memcpy((char *)mx_memcpy(
+                malloc(file->size + 5), " -> ", 4) + 4, buff, file->size + 1) - 4;
+        }
     }
 }
