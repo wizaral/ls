6117908024d37164f49993419d11c2411a322097
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-10 23:21:57 +0200

    try to do @e

--- LS/src/get/acl.c
+++ LS/src/get/acl.c
@@ -1,12 +1,35 @@
 #include "uls.h"
 
 void mx_acl(t_dir *dir, t_file *file, t_stat *st) {
-    acl_t acl = acl_get_file(dir->filename, ACL_TYPE_EXTENDED);
+    // acl_t acl1 = acl_get_file(dir->filename, ACL_TYPE_EXTENDED);
+    // acl_t acl2 = acl_get_file(dir->filename, 0);
+    // acl_t acl3 = acl_get_file(dir->filename, 1);
 
-    if (acl && ++st) {
-        file->fields.acl = acl_to_text(acl, NULL);
-        acl_free(acl);
-        // printf("%s\n", file->fields.acl);
-        // haven't done. need good text
-    }
+    // if (acl1) {
+    //     // file->fields.acl = acl_to_text(acl1, NULL);
+    //     char *acl = acl_to_text(acl1, NULL);
+
+    //     acl_free(acl1);
+    //     printf("1 %s\n", acl);
+    //     free(acl);
+    // }
+    // if (acl2) {
+    //     // file->fields.acl = acl_to_text(acl2, NULL);
+    //     char *acl = acl_to_text(acl2, NULL);
+
+    //     acl_free(acl2);
+    //     printf("2 %s\n", acl);
+    //     free(acl);
+    // }
+    // if (acl3) {
+    //     // file->fields.acl = acl_to_text(acl2, NULL);
+    //     char *acl = acl_to_text(acl3, NULL);
+
+    //     acl_free(acl3);
+    //     printf("3 %s\n", acl);
+    //     free(acl);
+    // }
+    ++dir;
+    ++st;
+    ++file;
 }
--- LS/src/get/attr.c
+++ LS/src/get/attr.c
@@ -2,14 +2,14 @@
 
 void mx_attr(t_dir *dir, t_file *file, t_stat *st) {
     if (listxattr(dir->filename, NULL, 0, XATTR_NOFOLLOW) > 0 && ++st) {
-        file->fields.attr = mx_strnew(1024);
-        listxattr(dir->filename, file->fields.attr, 1024, XATTR_NOFOLLOW);
-
-        // int l = getxattr(dir->filename, file->fields.attr, NULL, 1024, 1024, XATTR_NOFOLLOW);
-
+        // file->fields.attr = mx_strnew(1024);
+        // listxattr(dir->filename, file->fields.attr, 1024, XATTR_NOFOLLOW);
+        // int l = getxattr(dir->filename, file->fields.attr,
+        //                     NULL, 1024, 1024, XATTR_NOFOLLOW);
         // printf("%s : %i\n", file->fields.attr, l);
+        ++file;
 
         // haven't done
-        // need to concat l to attr. convert if -h flag
+        // need to concat l to attr
     }
 }
