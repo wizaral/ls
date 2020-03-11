715f8d885c0c4967d92c098eb7f14b0105d7948d
Oleksandr Afanasiev <oafanasiev@e2r1p3.unit.ua>
2020-03-11 16:37:40 +0200

    fix if statement

--- LS/libmx/src/list/mx_push_front.c
+++ LS/libmx/src/list/mx_push_front.c
@@ -4,7 +4,7 @@ void mx_push_front(t_list *list, void *data) {
     t_list_node *node = NULL;
 
     if (list && (node = mx_create_node(data, NULL, list->head))) {
-        if (list->head->prev)
+        if (list->head)
             list->head->prev = node;
         list->head = node;
         ++list->size;
