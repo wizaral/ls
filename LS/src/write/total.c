bff8c0ee14f40ed521d70894689c9e320d5a9a54
Alex Master <alex.macromaster@outlook.com>
2020-03-14 23:41:57 +0200

    fix total in empty dir

--- LS/src/write/total.c
+++ LS/src/write/total.c
@@ -6,8 +6,10 @@ void mx_nototal(t_info *info, t_dir *dir) {
 }
 
 void mx_total(t_info *info, t_dir *dir) {
-    mx_printstrlen("total ", 6, 1);
-    mx_printunum(dir->total, 1);
-    mx_printchar('\n', 1);
+    if (dir->array.size) {
+        mx_printstrlen("total ", 6, 1);
+        mx_printunum(dir->total, 1);
+        mx_printchar('\n', 1);
+    }
     ++info;
 }
